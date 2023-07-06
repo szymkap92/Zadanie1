2e8490388b68b9971a703546af62e3c92fb903d6
Szymon <Szymon@LAPTOP-0IOQFHRB>
2023-07-06 20:47:00 +0200

    Dodaj pliki projektów.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imieNazwisko = "Szymon" + "Kaput";
+            int wiek = 30;
+            char plec = 'm';
+            long pesel = 12340987135;
+            long numerPracownika = 2509324094;
+        }
+    }
+}
